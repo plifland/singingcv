e793e88aaed2f03f160f1a2f8a69344ebf61b9da
Peter Lifland <plifland@gmail.com>
2018-10-26 19:46:44 -0400

    modifying settings back

--- Django_SingingCV/settings.py
+++ Django_SingingCV/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/app/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
