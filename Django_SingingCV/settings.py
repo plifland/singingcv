3ba69d2f089abbdc0f463c261b15965063ef3cc3
Peter Lifland <plifland@gmail.com>
2018-10-26 19:54:43 -0400

    adding a manual static directory

--- Django_SingingCV/settings.py
+++ Django_SingingCV/settings.py
@@ -127,5 +127,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
+STATICFILES_DIRS = (
+    'D:/home/site/wwwroot/static',
+)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
