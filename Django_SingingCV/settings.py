c907389d3eeb221f73c849d1c76a0b1b1fede505
Peter Lifland <plifland@gmail.com>
2018-11-01 12:40:04 -0400

    tweaking static again - still not working in deployment

--- Django_SingingCV/settings.py
+++ Django_SingingCV/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/app/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/app/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'app/static/')
 STATICFILES_DIRS = (
     'D:/home/site/wwwroot/app/static',
 )
