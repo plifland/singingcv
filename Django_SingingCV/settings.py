e090fae49aa1be4aab3705fddbe43667894499bf
Peter Lifland <plifland@gmail.com>
2018-11-02 00:53:27 -0400

    changing static to default again

--- Django_SingingCV/settings.py
+++ Django_SingingCV/settings.py
@@ -125,10 +125,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/app/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'app/static/')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'app/static/')
+    #os.path.join(BASE_DIR, 'app/static/'),
     #'D:/home/site/wwwroot/app/static',
 )
 MEDIA_URL = '/media/'
