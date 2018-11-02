c682f4e85d1a0d5285be719517aefe3b841745c6
Peter Lifland <plifland@gmail.com>
2018-11-01 23:48:51 -0400

    undoing custom deployment editing staticfiles_dirs

--- Django_SingingCV/settings.py
+++ Django_SingingCV/settings.py
@@ -128,7 +128,8 @@ USE_TZ = True
 STATIC_URL = '/app/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'app/static/')
 STATICFILES_DIRS = (
-    'D:/home/site/wwwroot/app/static',
+    os.path.join(BASE_DIR, 'app/static/')
+    #'D:/home/site/wwwroot/app/static',
 )
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
